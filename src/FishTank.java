3864b1e23c1d0f6ad7ea2a9230a3644d87b4a5a8
PigeonPC <olgakatinka@gmail.com>
2025-02-26 11:45:08 +0100

    Comments

--- src/FishTank.java
+++ src/FishTank.java
@@ -1,11 +1,24 @@
+import java.time.LocalDateTime;
+
 public class FishTank {
     //Olga og Silke
 
+    //Attributes
     private String lastWaterChange;
     private String waterQuality;
 
+    //Constructor
     public FishTank(String lastWaterChange, String waterQuality){
         this.lastWaterChange = lastWaterChange;
         this.waterQuality = waterQuality;
+    }
+
+    LocalDateTime.now.changeWater()
+
+
+    //Change water method
+    public String changeWater(){
+
+        return lastWaterChange;
     }
 }
