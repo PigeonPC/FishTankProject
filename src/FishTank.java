6ac30d503f53bd56a86045b20e198d93e45d450b
PigeonPC <olgakatinka@gmail.com>
2025-02-26 11:20:28 +0100

    Comments

--- src/FishTank.java
+++ src/FishTank.java
@@ -1,3 +1,11 @@
 public class FishTank {
     //Olga og Silke
+
+    private String lastWaterChange;
+    private String waterQuality;
+
+    public FishTank(String lastWaterChange, String waterQuality){
+        this.lastWaterChange = lastWaterChange;
+        this.waterQuality = waterQuality;
+    }
 }
