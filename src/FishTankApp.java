2252c859bb2e6f31b33f1c1fd09d6783040cbc83
JB4000 <jabi0002@stud.kea.dk>
2025-02-26 11:47:51 +0100

    startet app

--- src/FishTankApp.java
+++ src/FishTankApp.java
@@ -1,2 +1,73 @@
 public class FishTankApp {
+
+    int menuChoice;
+
+
+    public static void main(String[] args) {
+
+        fishTankApp();
+
+
+    }
+
+    public static void fishTankApp() {
+
+        do {
+
+            printMenuOptions();
+            menuChoice = getMenuChoice();
+
+            if (menuChoice == 1) {
+                // TODO
+                // call viewing fish status
+            } else if (menuChoice == 2) {
+                // TODO
+                // call updating fish health
+            } else if (menuChoice == 3) {
+                // TODO
+                // call Changing  water
+            } else if (menuChoice == 4) {
+                // TODO
+                // call Viewing last water change
+            } else if (menuChoice == 5) {
+                // TODO
+                // Exit the this whole app
+                // fx med et break
+            }
+
+        } while (flagAppOn);
+
+
+
+
+
+
+
+
+    }
+
+
+    public static void printMenuOptions() {
+
+        // TODO
+        // Print menu options
+//        1.  Viewing fish status
+//        2.  Updating  fish health
+//        3.  Changing  water
+//        4.  Viewing last water change
+//        5.  Exiting
+        //System.out.println("fill out menu options here and ask for input");
+
+    }
+
+    public static int getMenuChoice() {
+
+        // TODO
+        // use scanner to get an int and validate the input range and type
+        // alternatively get input as a String
+        // return the choice as an int
+
+    }
+
+
 }
